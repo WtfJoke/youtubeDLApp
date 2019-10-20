7348094fe5193bf63ba79df44577ec72625959ed
Manuel <wtfjoke@livenet.ch>
2019-10-20 23:55:42 +0200

    :art: Add very basic UI

--- app/src/main/java/me/wessner/youtubedl/MainActivity.kt
+++ app/src/main/java/me/wessner/youtubedl/MainActivity.kt
@@ -2,11 +2,17 @@ package me.wessner.youtubedl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickDownload(view: View){
+        Toast.makeText(this, "Clicked on Download", Toast.LENGTH_LONG).show()
     }
 }
