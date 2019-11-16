76da156202e35f364cf8ddd6d4d9099ce773804e
Manuel <wtfjoke@livenet.ch>
2019-11-16 17:52:22 +0100

    :sparkles: :construction: Add preferences

--- app/src/main/java/me/wessner/youtubedl/ui/settings/SettingsFragment.kt
+++ app/src/main/java/me/wessner/youtubedl/ui/settings/SettingsFragment.kt
@@ -1,31 +1,12 @@
 package me.wessner.youtubedl.ui.settings
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.preference.PreferenceFragmentCompat
 import me.wessner.youtubedl.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    private lateinit var settingsViewModel: SettingsViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        settingsViewModel =
-            ViewModelProviders.of(this).get(SettingsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_settings, container, false)
-        val textView: TextView = root.findViewById(R.id.text_settings)
-        settingsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
-        return root
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
     }
 }
